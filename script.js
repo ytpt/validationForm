4953dc045c915e811047a37cab21bd0b1ac745f3
Annarchive <ytpt@mail.ru>
2023-03-24 21:28:35 +0800

    Send form without reloading in script.js; input and select validation when onblur in script.js; refactor code in form.html; add invalid and valid styles in style.css

---
+++ script.js
@@ -0,0 +1,68 @@
+const form = document.getElementById( "signUp");
+let inputs = document.querySelectorAll("input[data-rule]");
+let selects = document.querySelectorAll("select");
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+
+	if (formValidate(this)) {
+		console.log("Форма отправлена");
+	}
+});
+
+function formValidate() {
+	return true;
+}
+
+for (let input of inputs) {
+	input.addEventListener("blur", function() {
+		let rule = this.dataset.rule;
+		let value = this.value;
+		let check;
+
+		switch(rule) {
+			case "name":
+				check = /^[A-Za-z]+$/.test(value);
+				break;
+
+			case "email":
+				check = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+				break;
+
+			case "birthdate":
+				break;
+
+			case "gender":
+				break;
+
+			case "password":
+				check = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(value);
+				break;
+
+			case "confirm":
+				let pass1 = document.getElementById("password").value;
+				pass1 === value ? check = true : check = false;
+		}
+
+		this.classList.remove("invalid");
+		this.classList.remove("valid");
+
+		if (check) {
+			this.classList.add("valid");
+		} else {
+			this.classList.add("invalid");
+		}
+	})
+}
+
+for (let select of selects) {
+	select.addEventListener("blur", function() {
+
+		select.classList.remove("invalid");
+		select.classList.remove("valid");
+
+		select.value === "Default"
+			? select.classList.add("invalid")
+			: select.classList.add("valid");
+	})
+}
